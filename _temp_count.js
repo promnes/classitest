14069aec13bbea458158dd8284fcf1c5729260c3
dux1 <duxexch1@gmail.com>
2026-02-21 00:44:32 +0200

    cleanup: remove temp files

--- _temp_count.js
+++
@@ -1,6 +0,0 @@
-import fs from 'fs';
-const c = fs.readFileSync('./client/src/pages/SchoolProfile.tsx', 'utf8');
-const re = /t\("schoolProfile\.\w+",\s*"[^"]+"\)/g;
-const m = c.match(re);
-console.log('Found', m ? m.length : 0, 'matches');
-if (m) m.forEach((x, i) => console.log((i+1) + ': ' + x));
